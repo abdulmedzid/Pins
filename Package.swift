df579bded9609ffae0dea65a97f4fe309b52044b
Abdulmedzid Pehlic <abdulmedzidpehlic@outlook.com>
2024-10-11 20:43:19 +0200

    binary-distribution

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Pins",
-            targets: ["Pins"]),
+            targets: ["PinsKit"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Pins"),
-        .testTarget(
-            name: "PinsTests",
-            dependencies: ["Pins"]),
+        .binaryTarget(
+            name: "PinsKit",
+            path: "Sources/PinsKit.xcframework"
+        )
     ]
 )
---
+++ Sources/PinsKit.xcframework/ios-arm64/PinsKit.framework/Headers/PinsKit.h
@@ -0,0 +1,18 @@
+//
+//  PinsKit.h
+//  PinsKit
+//
+//  Created by Abdulmedzid Pehlic on 10. 10. 2024..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinsKit.
+FOUNDATION_EXPORT double PinsKitVersionNumber;
+
+//! Project version string for PinsKit.
+FOUNDATION_EXPORT const unsigned char PinsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinsKit/PublicHeader.h>
+
+
